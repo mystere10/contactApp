df3732aa77d60be533abcc19622a1b1c723d208a
Nkunzi Innocent <innocentnkunzi@yahoo.com>
2020-03-20 18:26:10 +0200

    added componentDidMout to fetch contacts

--- src/App.js
+++ src/App.js
@@ -1,32 +1,20 @@
 import React, { Component } from 'react';
 import ListContact from './ListContact'
-
+import * as contactsAPI from './utils/ContactsAPI';
 
 class App extends Component {
 
   state = {
-    contacts: [
-      {
-        "id": "karen",
-        "name": "Karen Isgrigg",
-        "handle": "karen_isgrigg",
-        "avatarURL": "http://localhost:5001/karen.jpg"
-      },
-      {
-        "id": "richard",
-        "name": "Richard Kalehoff",
-        "handle": "richardkalehoff",
-        "avatarURL": "http://localhost:5001/richard.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "handle": "tylermcginnis",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-     ]
+    contacts: []
   }
 
+  componentDidMount(){
+    contactsAPI.getAll().then((contacts) => {
+      this.setState(() => ({
+        contacts
+      }))
+    })
+  }
   removeContact = (contact) => {
     this.setState((currentState) => ({
       contacts: currentState.contacts.filter((c)=>{
